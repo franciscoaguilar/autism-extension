572fbb393f1c460745df7b9b2d08659f81749231
josephraguilar <josephraguilar@gmail.com>
2018-07-30 18:08:45 -0700

    some file layout and other stuff

--- console-log.js
+++ console-log.js
@@ -1,1 +1,11 @@
+
+document.body.style.color = "red";
+let p = document.body.getElementsByTagName('p');
+let a = document.body.getElementsByTagName('a');
+let ii = document.body.getElementsByTagName('i');
+// body.text.style.color = "red";
+
+
+
+
 console.log("we're in boys");
